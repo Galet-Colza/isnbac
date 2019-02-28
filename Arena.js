d84de63f2ea9feb88b0be66edf37340e86fa3b99
Galet-Colza <47414132+Galet-Colza@users.noreply.github.com>
2019-02-28 09:37:38 +0100

    Update Arena.js

--- Arena.js
+++ Arena.js
@@ -2,7 +2,9 @@ Arena = function(game) {
     // Appel des variables nécéssaires
     this.game = game;
     var scene = game.scene;
-
+        scene.collisionsEnabled = true
+        scene.gravity = new BABYLON.Vector3(0.1, 0.1, 0.1);
+    
     // Création de notre lumière principale
     var light = new BABYLON.PointLight("DirLight", new BABYLON.Vector3(0, 10, 0), scene);
 	light.diffuse = new BABYLON.Color3(1, 1, 1);
@@ -10,12 +12,14 @@ Arena = function(game) {
 	light.intensity = 1.5;
 
     // Créons une sphère 
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    // Remontons le sur l'axe y de la moitié de sa hauteur
-    sphere.position.y = 1;
-
+    var playerBox = BABYLON.Mesh.CreateBox("headMainPlayer", 3, scene);
+        playerBox.position = new BABYLON.Vector3(-20, 5, 5);
+        playerBox.ellipsoid = new BABYLON.Vector3(2, 2, 2);
+        playerBox.checkCollisions = true;
+        
     // Ajoutons un sol pour situer la sphere dans l'espace
     var ground = BABYLON.Mesh.CreateGround("ground1", 20, 20, 20, scene);
-
+        ground.checkCollisions = true;
 };
+
+
